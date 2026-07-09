365f34003622f4e01af4f5f130fbde740d4c0365
Dhruvik Nimavat <dhruvikalphabit07@gmail.com>
2026-07-09 14:28:18 +0530

    add my project

---
+++ contact.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Wedding Planner</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.materialdesignicons.com/2.1.19/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/datepicker.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/carousel.css">
+    <style>
+        body {
+            font-family: 'Open Sans', 'Roboto', sans-serif;
+            line-height: 1.5em;
+            margin-bottom: 2%;
+            margin-top: 3%;
+            width: 100%;
+            overflow-x: hidden;
+            background: #f1f1f1;
+        }
+
+        .navbar-light .navbar-brand {
+            color: #1a1a1a;
+            font-weight: bold;
+            line-height: 22px;
+        }
+
+        .navbar {
+            font-weight: 700;
+            padding: 12px;
+            font-style: normal;
+            font-size: 14px;
+            text-transform: uppercase;
+            color: black;
+            border-bottom: 1px solid #ddd;
+        }
+
+        li.nav-item > a.nav-link {
+            color: black !important;
+            font-weight: bold !important;
+        }
+
+        #review {
+            font-size: 16px;
+            font-weight: bold;
+            margin-right: 5px;
+        }
+
+        .form-inline > a.mr-2, .btn.btn-sm.my-2.my-sm-0 {
+            color: black;
+            font-size: 14px;
+            font-weight: 700;
+            margin-left: 10px;
+        }
+
+        .form-inline > a.mr-2:hover, .btn.btn-sm.my-2.my-sm-0:hover {
+            color: #17b4bc;
+            text-decoration: none;
+        }
+
+        a.btn.btn-sm.my-2.my-sm-0.mr-2.loginbtn {
+            background: #dc3545;
+            font-size: 14px;
+            color: white;
+            padding: 5px;
+            border: 2px solid transparent;
+            width: 85px;
+        }
+
+        a.btn.btn-sm.my-2.my-sm-0.mr-2.loginbtn:hover {
+            background: white;
+            border: 2px solid #dc3545;
+            color: #dc3545;
+        }
+
+        .navbar-expand-lg .navbar-nav .nav-link {
+            padding-right: .9rem;
+        }
+
+        .navbar-brand {
+            margin-left: 20px;
+            width: 200px;
+        }
+
+        .form-control {
+            font-size: 12px;
+            border-radius: 0;
+            margin-top: 0;
+        }
+
+
+
+    </style>
+</head>
+<body>
+<?php include "include/nav.php"; ?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3 class="text-center mb-3">CONTACT US</h3>
+            <div class="bg-white p-4">
+                <div class="contact-information">
+                    <h5>OFFICE LOCATION</h5>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <table class="table table-sm" style="font-size: 12px;">
+                                <tr>
+                                    <td>Event planner</td>
+                                    <td><i class="mdi mdi-deskphone mr-3"></i> (0281)-6356727</td>
+                                </tr>
+                                <tr>
+                                    <td>5th floor, Shree Ram Building</td>
+                                    <td><i class="mdi mdi-phone mr-3"></i> +91 6356727015</td>
+                                </tr>
+                                <tr>
+                                    <td>Street-2 Om Shanti Park</td>
+                                    <td><i class="mdi mdi-email mr-3"></i> dhruviknimavat7@gmail.com</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="text-center mt-3">
+                                <i class="mdi mdi-map-marker" style="font-size: 110px;color: #22ADB5;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- MODAL LOGIN SECTION-->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="loginModal"><img src="images/logo/MOMMYAMBOL.png" alt=""></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
